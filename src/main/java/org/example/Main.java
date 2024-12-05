2bfef93000045074a003cb2b133e32b202a21f4a
AccessCoder <info.unaddict@gmail.com>
2024-12-05 09:51:18 +0100

    add greetings for Feature2 and 3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,5 +6,7 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Hello Test");
         System.out.println("Hello Feature");
+        System.out.println("Hello Feature2");
+        System.out.println("Hello Feature3");
     }
 }
